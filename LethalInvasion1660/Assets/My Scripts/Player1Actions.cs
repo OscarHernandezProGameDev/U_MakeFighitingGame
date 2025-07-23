142d8c2cc212fbebe6241f3ec1dc4ad05285cc5b
Oscar Devs <ohernandezba@gmail.com>
2025-07-23 23:22:52 +0200

    Spinning Jump

--- LethalInvasion1660/Assets/My Scripts/Player1Actions.cs
+++ LethalInvasion1660/Assets/My Scripts/Player1Actions.cs
@@ -12,15 +12,27 @@ public class Player1Actions : MonoBehaviour
         Player1.transform.Translate(0, JumpSpeed, 0);
     }
 
+    public void FlipUp()
+    {
+        Player1.transform.Translate(0, JumpSpeed, 0);
+        Player1.transform.Translate(0.2f, 0, 0);
+    }
+
+    public void FlipBack()
+    {
+        Player1.transform.Translate(0, JumpSpeed, 0);
+        Player1.transform.Translate(-0.2f, 0, 0);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
