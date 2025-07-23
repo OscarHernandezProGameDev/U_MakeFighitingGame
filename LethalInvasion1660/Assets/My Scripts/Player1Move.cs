9cb34bcc671af7af360aa17515a8adcfb15fda11
Oscar Devs <ohernandezba@gmail.com>
2025-07-23 18:35:51 +0200

    Cannot exit screen

--- LethalInvasion1660/Assets/My Scripts/Player1Move.cs
+++ LethalInvasion1660/Assets/My Scripts/Player1Move.cs
@@ -9,6 +9,8 @@ public class Player1Move : MonoBehaviour
     public float WalkSpeed = 0.001f;
     private bool isJumping = false;
     private AnimatorStateInfo Player1Layer0;
+    private bool CanWalkLeft = true;
+    private bool CanWalkRight = true;
 
     // Start is called before the first frame update
     void Start()
@@ -20,18 +22,41 @@ public class Player1Move : MonoBehaviour
     void Update()
     {
         Player1Layer0 = Anim.GetCurrentAnimatorStateInfo(0);
+
+        Vector3 ScreenBounds = Camera.main.WorldToScreenPoint(this.transform.position);
+
+        if (ScreenBounds.x > Screen.width - 200)
+        {
+            CanWalkRight = false;
+        }
+        else if (ScreenBounds.x < 200)
+        {
+            CanWalkLeft = false;
+        }
+        else
+        {
+            CanWalkLeft = true;
+            CanWalkRight = true;
+        }
+
         // El tag nos indicará si nos podemos mover o no
         if (Player1Layer0.IsTag("Motion"))
         {
             if (Input.GetAxis("Horizontal") > 0)
             {
-                Anim.SetBool("Forward", true);
-                transform.Translate(WalkSpeed, 0, 0);
+                if (CanWalkRight)
+                {
+                    Anim.SetBool("Forward", true);
+                    transform.Translate(WalkSpeed, 0, 0);
+                }
             }
             if (Input.GetAxis("Horizontal") < 0)
             {
-                Anim.SetBool("Backward", true);
-                transform.Translate(-WalkSpeed, 0, 0);
+                if (CanWalkLeft)
+                {
+                    Anim.SetBool("Backward", true);
+                    transform.Translate(-WalkSpeed, 0, 0);
+                }
             }
         }
         if (Input.GetAxis("Horizontal") == 0)
