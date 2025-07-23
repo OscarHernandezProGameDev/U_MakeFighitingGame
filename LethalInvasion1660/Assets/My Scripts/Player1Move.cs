72dc3de5f6be09fb76d17e398c870126e13c88e6
Oscar Devs <ohernandezba@gmail.com>
2025-07-23 18:19:48 +0200

    Jumping

--- LethalInvasion1660/Assets/My Scripts/Player1Move.cs
+++ LethalInvasion1660/Assets/My Scripts/Player1Move.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,8 @@ public class Player1Move : MonoBehaviour
 {
     private Animator Anim;
     public float WalkSpeed = 0.001f;
+    private bool isJumping = false;
+    private AnimatorStateInfo Player1Layer0;
 
     // Start is called before the first frame update
     void Start()
@@ -16,15 +19,20 @@ public class Player1Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxis("Horizontal") > 0)
+        Player1Layer0 = Anim.GetCurrentAnimatorStateInfo(0);
+        // El tag nos indicará si nos podemos mover o no
+        if (Player1Layer0.IsTag("Motion"))
         {
-            Anim.SetBool("Forward", true);
-            transform.Translate(WalkSpeed, 0, 0);
-        }
-        if (Input.GetAxis("Horizontal") < 0)
-        {
-            Anim.SetBool("Backward", true);
-            transform.Translate(-WalkSpeed, 0, 0);
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                Anim.SetBool("Forward", true);
+                transform.Translate(WalkSpeed, 0, 0);
+            }
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                Anim.SetBool("Backward", true);
+                transform.Translate(-WalkSpeed, 0, 0);
+            }
         }
         if (Input.GetAxis("Horizontal") == 0)
         {
@@ -34,7 +42,13 @@ public class Player1Move : MonoBehaviour
 
         if (Input.GetAxis("Vertical") > 0)
         {
-            Anim.SetTrigger("Jump");
+            if (!isJumping)
+            {
+                isJumping = true;
+                Anim.SetTrigger("Jump");
+                StartCoroutine(JumpPause());
+            }
+
         }
         if (Input.GetAxis("Vertical") < 0)
         {
@@ -44,5 +58,11 @@ public class Player1Move : MonoBehaviour
         {
             Anim.SetBool("Crouch", false);
         }
+    }
+
+    private IEnumerator JumpPause()
+    {
+        yield return new WaitForSeconds(1.0f);
+        isJumping = false;
     }
 }
